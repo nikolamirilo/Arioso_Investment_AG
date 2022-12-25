bc2a8858dcb25c59617419c4e1a69c4df3c75ca5
Nikola Mirilo <nikolamirilo@gmail.com>
2022-12-25 11:58:33 +0100

    added animations

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
   return (
     <div className="layout">
       <div className="navbar-container">
         <Navbar />
       </div>
-      <main className="app">{children}</main>
+      <main className="app" style={{ minHeight: location.pathname !== "/our-team" ? "200vh" : "120vh" }}>
+        {children}
+      </main>
       <div className="footer-container">
         <Footer />
       </div>
--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = ({ children }) => {
   const location = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, [location]);
 
   return <>{children}</>;
