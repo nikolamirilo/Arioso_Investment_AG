7a3cab683981d9125bb9d7f67706b160f878f155
Nikola Mirilo <nikolamirilo@gmail.com>
2022-12-26 00:41:48 +0100

    removed animation for hamburger

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -13,9 +13,17 @@ const Form = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    const nameInput = document.getElementById("nameInput");
+    const emailInput = document.getElementById("emailInput");
+    const subjectInput = document.getElementById("subjectInput");
+    const messageInput = document.getElementById("messageInput");
     emailjs.sendForm(service_id, template_id, form.current, public_key).then(
       (result) => {
         setIsFormSubmited(true);
+        nameInput.value = "";
+        emailInput.value = "";
+        subjectInput.value = "";
+        messageInput.value = "";
       },
       (error) => {
         alert(`Error`);
@@ -26,11 +34,16 @@ const Form = () => {
   return (
     <form className="form" ref={form} onSubmit={handleFormSubmit}>
       <h1>{t("Contact Us")}</h1>
-      <input type="hidden" value="contact" />
-      <input type="text" placeholder={t("Enter your name*")} name="name" required={true} />
-      <input type="email" placeholder={t("Enter your email address*")} name="email" required={true} />
-      <input type="text" placeholder={t("Subject*")} name="subject" required={true} />
-      <textarea type="text" required={true} name="message" placeholder={t("Enter your message*")}></textarea>
+      <input type="text" id="nameInput" placeholder={t("Enter your name*")} name="name" required={true} />
+      <input type="email" id="emailInput" placeholder={t("Enter your email address*")} name="email" required={true} />
+      <input type="text" id="subjectInput" placeholder={t("Subject*")} name="subject" required={true} />
+      <textarea
+        type="text"
+        id="messageInput"
+        required={true}
+        name="message"
+        placeholder={t("Enter your message*")}
+      ></textarea>
       <button type="submit">{t("Send")}</button>
     </form>
   );
