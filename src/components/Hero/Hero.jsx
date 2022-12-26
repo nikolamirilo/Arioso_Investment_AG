e5c33baeaa5b825fe2f1d117c223bcb54c87e0a1
Nikola Mirilo <nikolamirilo@gmail.com>
2022-12-26 01:08:16 +0100

    added background color to heading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Hero = (props) => {
   return (
     <div className="hero" style={{ backgroundImage: `url(${props.image})` }}>
-      <h1>{props.title}</h1>
-      {props.subtitle ? <p>{props.subtitle}</p> : null}
-      {props.button ? <button>{props.button}</button> : null}
+      <div className="heading">
+        <h1>{props.title}</h1>
+        {props.subtitle ? <p>{props.subtitle}</p> : null}
+        {props.button ? <button>{props.button}</button> : null}
+      </div>
     </div>
   );
 };
