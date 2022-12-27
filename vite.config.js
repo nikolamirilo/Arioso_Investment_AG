d49c95fe820118fabc67739bfb6f44c8cfb44b81
nikolamirilo <nikolamirilo@gmail.com>
2022-12-27 11:49:49 +0100

    chnaged vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  },
   build: {
     reportCompressedSize: true,
     chunkSizeWarningLimit: 500,
